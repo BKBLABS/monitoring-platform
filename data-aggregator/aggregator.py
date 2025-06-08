da9b28d11d91adc2b857951decf0b9a5112a6399
Aditya Malik <102025295+AdityaMalik7@users.noreply.github.com>
2025-06-08 14:58:04 +0530

    Update aggregator.py

--- data-aggregator/aggregator.py
+++ data-aggregator/aggregator.py
@@ -8,7 +8,7 @@ def fetch_hyphenmon():
 
 def aggregate():
     zabbix = ZabbixClient("http://192.168.1.7/api_jsonrpc.php", "Admin", "zabbix")
-    z_data = zabbix.get_items("10105")  # Replace with your hostid
+    z_data = zabbix.get_items("10105")  
     h_data = fetch_hyphenmon()
     return {"zabbix": z_data, "hyphenmon": h_data}
 
